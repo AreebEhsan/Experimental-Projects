04a1ea6ada9b264e65b6f1e444db84a8517fcfbe
Areeb <49764279+AreebEhsan@users.noreply.github.com>
2026-04-06 23:21:15 -0400

    Experimenting and setting up mcp servers with tools, resources and prompts

---
+++ mcp-servers/basic-mcp-server/server.py
@@ -0,0 +1,26 @@
+from mcp.server.fastmcp import FastMCP
+
+# Creating the mcp server
+mcp = FastMCP("basic-server")
+
+@mcp.tool()
+def add(a: int, b:int) -> int:
+    """Adding two numbers"""
+    return a + b
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiplying two numbers"""
+    return a * b
+
+@mcp.resource("notes://welcome")
+def welcome_note():
+    return "This is the first MCP resource"
+
+@mcp.prompt()
+def research_assistant(topics: list[str]) -> str:
+    return "You are a research assistant.\n\n" + "\n".join(
+        f"Researching: {topic}" for topic in topics
+    )
+if __name__ == "__main__":
+    mcp.run()
